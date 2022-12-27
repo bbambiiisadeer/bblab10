f38822829b28aca670f838a6543b7df379a3c05d
bbam <nunnapats1822@gmail.com>
2022-12-27 16:38:17 +0700

    lan10_2completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int N,int M){
+    if(N<=0||M<=0){
+        cout<<"Invalid input";}
+    for(int i=0;i<N;i++){
+        if(M>0){
+            for(int j=0;j<M;j++){
+                cout<<'O';
+            }
+            cout<<"\n";
+        }
+        
+    }
+    
+}
 
 int main(){
 	
